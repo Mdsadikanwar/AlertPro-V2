1196b8b92377ae1e404c7bf5d9046224d0187ac4
Mdsadikanwar <91738121+Mdsadikanwar@users.noreply.github.com>
2026-07-09 11:49:55 +0400

    Add configuration settings for Telegram and CoinGecko

---
+++ assets/js/settings.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+    TELEGRAM_TOKEN: "PASTE_YOUR_TOKEN_HERE",
+    CHAT_ID: "PASTE_YOUR_CHAT_ID_HERE",
+    COINGECKO_API: "https://api.coingecko.com/api/v3"
+}
+let SIGNAL_LOGS = JSON.parse(localStorage.getItem('apex_logs')) || [];
